7d8f952c4c976993cbababfbb00e73fe0d365f84
“sv-zlf” <1375451299@qq.com>
2023-01-29 14:27:40 +0800

    20230129

---
+++ _230129/src/BinarySearch.java
@@ -0,0 +1,34 @@
+/**
+ * @author zlf
+ * @data 2023/1/29
+ * @apiNote 二分查找
+ * 给定一个n个元素有序的（升序）整型数组nums和一个目标值target
+ * 写一个函数搜索nums中的target，如果目标值存在返回下标，否则返回-1。
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int nums[]={-1,0,3,5,9,12};
+        int target = 9;
+        System.out.println(search(nums,target));
+    }
+
+    public static int search(int[] nums, int target) {
+        int right=nums.length-1;
+        int left=0;
+        while (left<=right){
+            int mid =(right+left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>target){
+               right=mid-1;
+            }
+            if(nums[mid]<target){
+                left=mid+1;
+            }
+            //System.out.println("right:"+right+"left:"+left);
+        }
+        return -1;
+    }
+}
--- _230129/src/test.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author zlf
- * @data 2023/1/29
- * @apiNote
- */
-public class test {
-}
