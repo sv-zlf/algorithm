aa682977c68bba62e845ef3c0455594c2af64e6b
“sv-zlf” <1375451299@qq.com>
2023-01-29 11:21:48 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author zlf
+ * @data ${DATE}
+ * @apiNote
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(change());
+    }
+
+    public static String change(){
+        return "2023年1月29日改变开始";
+    }
+}
---
+++ src/_23_01_29/TEST.java
@@ -0,0 +1,9 @@
+package _23_01_29;
+
+/**
+ * @author zlf
+ * @data 2023/1/29
+ * @apiNote
+ */
+public class TEST {
+}
